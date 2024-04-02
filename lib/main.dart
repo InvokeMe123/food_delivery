dfc7232210b630257cf3b8a4d452a17e019e65d1
Aadarsh Ghimire <aadarshghimire524@gmail.com>
2024-04-03 00:23:54 +0545

    initiated_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,20 @@ import 'package:food_delivery/core/app_initialize.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:food_delivery/wrapper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await AppInitialize.appInitialize();
   await SharedPreferences.getInstance();
   await EasyLocalization.ensureInitialized();
   runApp(ProviderScope(
       child: EasyLocalization(
           path: 'assets/translations',
-          fallbackLocale:  Locale("en", "US"),
-          supportedLocales:  [Locale("en", "US"), Locale("ar", "AE")],
-          child:  FoodDeliveryApp())));
+          fallbackLocale: Locale("en", "US"),
+          supportedLocales: [Locale("en", "US"), Locale("ar", "AE")],
+          child: FoodDeliveryApp())));
 }
